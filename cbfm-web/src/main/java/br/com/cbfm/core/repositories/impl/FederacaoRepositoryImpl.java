250bd8d7dc975f677ca3498d00d2c2ebedc011cb
luiscamposjr <luiscamposjr@gmail.com>
2016-04-09 20:14:21 -0300

    Java Doc features.

--- cbfm-web/src/main/java/br/com/cbfm/core/repositories/impl/FederacaoRepositoryImpl.java
+++ cbfm-web/src/main/java/br/com/cbfm/core/repositories/impl/FederacaoRepositoryImpl.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package br.com.cbfm.core.repositories.impl;
 
 import java.util.List;
@@ -35,7 +20,7 @@ import br.com.cbfm.core.repositories.FederacaoRepositoryCustom;
 /**
  * Custom implementation class to implement {@link FederacaoRepositoryCustom}. Using the Querydsl repository base class.
  * 
- * @author Oliver Gierke
+ * @author Luis Campos
  */
 public class FederacaoRepositoryImpl extends QueryDslRepositorySupport implements FederacaoRepositoryCustom {
 	
