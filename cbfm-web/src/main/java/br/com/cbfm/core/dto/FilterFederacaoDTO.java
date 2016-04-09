42ac176878b531f121216bd59d6d47215c53d667
luiscamposjr <luiscamposjr@gmail.com>
2016-04-09 20:10:33 -0300

    Added AbstractEntity to be super persitable class. Added FilterFederacaoDTO to be used by the filter on the crud screen.

---
+++ cbfm-web/src/main/java/br/com/cbfm/core/dto/FilterFederacaoDTO.java
@@ -0,0 +1,28 @@
+package br.com.cbfm.core.dto;
+
+public class FilterFederacaoDTO {
+	
+	private String sigla;
+	private String uf;
+	private String nome;
+	
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
